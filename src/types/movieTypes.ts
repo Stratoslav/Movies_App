4a00e05c8a6975e746a601cbf79761993d89c60a
Stratoslav <stas.kurbanov03@gmail.com>
2023-02-24 19:50:15 +0300

    G

---
+++ src/types/movieTypes.ts
@@ -0,0 +1,20 @@
+import { AllMovies } from './AllMovies';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { movies: [] as AllMovies[], searchedMovie: [] };
+
+const MoviesSlice = createSlice({
+  name: 'movies/get',
+  initialState,
+  reducers: {
+    getPopularMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+    getSearchMovies: (state, action) => {
+      state.searchedMovie = action.payload;
+    }
+  }
+});
+
+export const { reducer: popularMoviesReducer, actions: popularMoviesAction } =
+  MoviesSlice;
