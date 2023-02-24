6e082722addc662e62bee28b4da49c080eda8519
Stratoslav <stas.kurbanov03@gmail.com>
2023-02-24 17:39:36 +0300

    F

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import './normalize.css';
+import './normalize.scss';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
